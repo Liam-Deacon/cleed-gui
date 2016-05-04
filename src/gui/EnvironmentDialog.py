5679d118ad92348e74eb4526d6a22f576fb1b013
Liam Deacon <liam.m.deacon@gmail.com>
2016-05-04 22:35:53 +0100

    Updated Environment dialog with further functionality

--- src/gui/EnvironmentDialog.py
+++ src/gui/EnvironmentDialog.py
@@ -31,7 +31,7 @@ from PyQt4 import QtGui, uic
 import res_rc
 import os
 import sys
-
+from copy import deepcopy
 
 class EnvironmentDialog(QtGui.QDialog):
     '''
@@ -61,25 +61,82 @@ class EnvironmentDialog(QtGui.QDialog):
         self.ui.show()
         
         self.env = self.DEFAULTS 
+        self.environ = deepcopy(os.environ)
             
     def initUi(self):
         # Setup slots and signals
         self.ui.buttonBox.clicked[QtGui.QAbstractButton].connect(self.buttonPress)
         
-        self.ui.addVariableButton.clicked.connect(lambda: sys.stderr.write("TODO\n"))
+        self.ui.addVariableButton.clicked.connect(self.addVariable)
         self.ui.deleteVariableButton.clicked.connect(self.deleteVariable)
         
-        self.ui.showFullEnvironmentCheckBox.stateChanged.connect(lambda: sys.stderr.write("TODO\n"))
+        self.ui.showFullEnvironmentCheckBox.toggled.connect(self.showAll)
         self.ui.systemEnvironmentCheckBox.stateChanged.connect(lambda: sys.stderr.write("TODO\n"))
         
+        self.ui.tableWidget.cellChanged.connect(self.updateVariable)
         self.ui.tableWidget.verticalHeader().setVisible(True)
     
+    def updateVariable(self, row, col):
+        var = str(self.ui.tableWidget.verticalHeaderItem(row).text())
+        value = str(self.ui.tableWidget.item(row, col).text())
+        self.environ[var] = value
+    
+    def showAll(self, toggled):
+        if toggled:
+            rows = set(list(self.environ.keys()) + list(os.environ.keys()))
+        else:
+            rows = set(self.environ.keys()) - set(os.environ.keys())
+        
+        headers = [str(self.ui.tableWidget.verticalHeaderItem(i)) 
+                   for i in range(self.ui.tableWidget.rowCount())]
+        for label in rows:
+            if label not in headers:
+                self.ui.tableWidget.insertRow(self.ui.tableWidget.rowCount())
+                
+    
     def addVariable(self):
-        pass
+        new_var, ok = QtGui.QInputDialog.getText(self, "New variable...", 
+                                                 "Enter Variable Name")
+        
+        # add new variable if not already in list
+        if ok and str(new_var).upper() not in (key.upper() 
+                                               for key in self.vars.keys()):
+            row = self.ui.tableWidget.rowCount()
+            item = QtGui.QTableWidgetItem(str(new_var))
+            self.ui.tableWidget.insertRow(row)
+            self.ui.tableWidget.setVerticalHeaderItem(row, item)
+            self.environ[str(new_var)] = ''
+
+    @property
+    def vars(self):
+        return dict((str(self.ui.tableWidget.verticalHeaderItem(row).text()), 
+                     str(self.ui.tableWidget.item(row, 0).text())
+                     if self.ui.tableWidget.item(row, 0) else '') 
+                    for row in range(self.ui.tableWidget.rowCount()))
     
     def deleteVariable(self):
-        active_var = self.ui.tableWidget.verticalHeaderItem(self.ui.tableWidget.currentRow()).text()
-        print('TODO: delete ' + active_var)
+        row = self.ui.tableWidget.currentRow()
+        active_var = str(self.ui.tableWidget.verticalHeaderItem(row).text())
+        del_msg = "Do you want to delete environment variable '{}'?".format(active_var)
+        reply = QtGui.QMessageBox.question(self, 
+                                           'Message', 
+                                           del_msg, 
+                                           QtGui.QMessageBox.Yes, 
+                                           QtGui.QMessageBox.No)
+
+        if (reply == QtGui.QMessageBox.Yes and 
+            active_var not in self.DEFAULTS.keys()):
+            self.ui.tableWidget.removeRow(row)
+            del(self.environ[active_var])
+            print(self.environ)
+        elif active_var in self.DEFAULTS.keys():
+            info_msg = "Cannot delete variable '{}': read-only".format(active_var)
+            QtGui.QMessageBox.information(self, 
+                                          'Info', 
+                                          info_msg, 
+                                          QtGui.QMessageBox.Ok)
+
+        
     
     def buttonPress(self, button):
         '''Deal with user interaction of button group'''
