803ec19f268852f1c11abd852ae5968ca44c427d
Liam Deacon <liam.m.deacon@gmail.com>
2016-06-15 20:54:27 +0100

    Added skeletons for ColorComboBox and ColorButton classes

--- src/gui/widgets.py
+++ src/gui/widgets.py
@@ -1,28 +1,60 @@
 from qtbackend import QtGui, QtCore
+from collections import OrderedDict
+
+try:
+    from matplotlib.backends.qt_editor.formlayout import ColorButton
+except ImportError:
+    import sys
+    sys.stderr.write("Could not load ColorButton from matplotlib")
+    class ColorButton(QtGui.QPushButton):
+        """ Create crude approximation of ColorButton """
+        def __init__(self, parent=None, color=None):
+            super(ColorButton, self).__init__(parent)            
+        
+class ColorComboBox(QtGui.QComboBox):
+    """ A combo box for selecting a color """
+    def __init__(self, parent=None, color=None):
+        super(ColorComboBox, self).__init__(parent)
+        
+        self.addItems([str(color) for color in QtGui.QColor.colorNames()] + 
+                      ['<Custom Color>'])
+        
+        self.color = color
+    
+    @property
+    def color(self):
+        return self._color
+    
+    @color.setter
+    def color(self, color):
+        self._color = color
+
 
 class BrushWidget(QtGui.QWidget):
     """ Widget class for controlling brush options """
     
-    FILL_STYLES = {'Solid': QtCore.Qt.SolidPattern,
-                   'Linear Gradient': QtCore.Qt.LinearGradientPattern,
-                   'Conical Gradient': QtCore.Qt.ConicalGradientPattern,
-                   'Radial Gradient': QtCore.Qt.RadialGradientPattern,
-                   'Texture': QtCore.Qt.TexturePattern,
-                   'Horizontal': QtCore.Qt.HorPattern,
-                   'Vertical': QtCore.Qt.VerPattern,
-                   'Cross': QtCore.Qt.CrossPattern,
-                   'Backward Diagonal': QtCore.Qt.BDiagPattern,
-                   'Forward Diagonal': QtCore.Qt.FDiagPattern,
-                   'Diagonal Cross': QtCore.Qt.DiagCrossPattern,
-                   'Dense 1': QtCore.Qt.Dense1Pattern,
-                   'Dense 2': QtCore.Qt.Dense2Pattern,
-                   'Dense 3': QtCore.Qt.Dense3Pattern,
-                   'Dense 4': QtCore.Qt.Dense4Pattern,
-                   'Dense 5': QtCore.Qt.Dense5Pattern,
-                   'Dense 6': QtCore.Qt.Dense6Pattern,
-                   'Dense 7': QtCore.Qt.Dense7Pattern,
-                   'None': QtCore.Qt.NoBrush,
-                   }
+    FILL_STYLES = OrderedDict((
+                    ('Solid', QtCore.Qt.SolidPattern),
+                    ('Linear Gradient', QtCore.Qt.LinearGradientPattern),
+                    ('Conical Gradient', QtCore.Qt.ConicalGradientPattern),
+                    ('Radial Gradient', QtCore.Qt.RadialGradientPattern),
+                    ('Texture', QtCore.Qt.TexturePattern),
+                    ('Horizontal', QtCore.Qt.HorPattern),
+                    ('Vertical', QtCore.Qt.VerPattern),
+                    ('Cross', QtCore.Qt.CrossPattern),
+                    ('Backward Diagonal', QtCore.Qt.BDiagPattern),
+                    ('Forward Diagonal', QtCore.Qt.FDiagPattern),
+                    ('Diagonal Cross', QtCore.Qt.DiagCrossPattern),
+                    ('Dense 1', QtCore.Qt.Dense1Pattern),
+                    ('Dense 2', QtCore.Qt.Dense2Pattern),
+                    ('Dense 3', QtCore.Qt.Dense3Pattern),
+                    ('Dense 4', QtCore.Qt.Dense4Pattern),
+                    ('Dense 5', QtCore.Qt.Dense5Pattern),
+                    ('Dense 6', QtCore.Qt.Dense6Pattern),
+                    ('Dense 7', QtCore.Qt.Dense7Pattern),
+                    ('None', QtCore.Qt.NoBrush),
+                    )
+                )
     
     def __init__(self, parent=None, brush=None):
         super(self.__class__, self).__init__(parent)
@@ -37,6 +69,20 @@ class BrushWidget(QtGui.QWidget):
         fill_combo = QtGui.QComboBox()
         fill_combo.addItems(self.FILL_STYLES.keys())
         layout.addWidget(fill_combo, 1, 1)
+        
+        color_label = QtGui.QLabel("Fill Color")
+        layout.addWidget(color_label, 2, 0)
+        color_combo = ColorComboBox()
+        layout.addWidget(color_combo, 2, 1)
+        fill_color = ColorButton()
+        layout.addWidget(fill_color, 2, 2)
+
+        color_label_2 = QtGui.QLabel("Fill Color 2")
+        layout.addWidget(color_label_2, 3, 0)
+        color_combo_2 = ColorComboBox()
+        layout.addWidget(color_combo_2, 3, 1)
+        fill_color_2 = ColorButton()
+        layout.addWidget(fill_color_2, 3, 2)
         
         self.setLayout(layout)
         
@@ -93,8 +139,10 @@ class PenWidget(QtGui.QWidget):
         
         color_label = QtGui.QLabel("Pen Color")
         layout.addWidget(color_label, 5, 0)
-        color_combo = QtGui.QComboBox()
+        color_combo = ColorComboBox()
         layout.addWidget(color_combo, 5, 1)
+        color_button = ColorButton()
+        layout.addWidget(color_button, 5, 2)
         
         self.setLayout(layout)
         
